240eb9c96f452e6c2e95d1dbeb304a8ff5a10404
21e8 <alize_in_cryptoland@protonmail.com>
2024-03-28 19:42:31 +0100

    init

---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const dotenv = require("dotenv");
+dotenv.config();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://boden-tremp-2024.vercel.app",
+      "https://www.bodentremp2024.com",
+    ],
+  })
+);
+
+const config = {
+  aMint: "3psH1Mj1f7yUfaD5gh6Zj7epE8hhrMkMETgv5TshQA4o",
+  aDescriptor: "boden",
+  bMint: "FU1q8vJpZNUrmqsciSjp8bAKKidGsLmouB8CBdf8TKQv",
+  bDescriptor: "tremp",
+};
+/**
+ * @typedef {Object} TokenAccount
+ * @property {string} address
+ * @property {string} mint
+ * @property {string} owner
+ * @property {number} amount
+ * @property {number} delegated_amount
+ * @property {boolean} frozen
+ */
+const bodenToken = "3psH1Mj1f7yUfaD5gh6Zj7epE8hhrMkMETgv5TshQA4o";
+const trempToken = "FU1q8vJpZNUrmqsciSjp8bAKKidGsLmouB8CBdf8TKQv";
+const rpcUrl = process.env.RPC_URL || "https://api.mainnet-beta.solana.com";
+const holders = {
+  [config.aDescriptor]: 0,
+  [config.bDescriptor]: 0,
+};
+/**
+ *
+ * @param {'boden' | 'tremp'} key
+ * @returns {Map<string, TokenAccount>}
+ */
+const findHolders = async (key) => {
+  let page = 1;
+  let allOwners = new Map();
+  const address = key === config.aDescriptor ? config.aMint : config.bMint;
+
+  try {
+    while (true) {
+      const response = await fetch(rpcUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          method: "getTokenAccounts",
+          id: "bar",
+          params: {
+            page: page,
+            limit: 1000,
+            displayOptions: {},
+            mint: address,
+          },
+        }),
+      }).catch((e) => {
+        console.log(e);
+      });
+      if (response.status !== 200) {
+        console.log(`Error fetching data. Status: ${response.status}`);
+        break;
+      }
+      const data = await response.json();
+
+      if (!data.result || data.result.token_accounts.length === 0) {
+        console.log(`No more results. Total pages: ${page - 1}`);
+
+        break;
+      }
+      data.result.token_accounts.forEach((account) => {
+        if (account.amount > 0) {
+          allOwners.set(account.owner, account);
+        }
+      });
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      page++;
+    }
+    console.log(`Total holders: ${allOwners.size}`);
+    return allOwners.size;
+  } catch (error) {
+    console.error(error);
+    return holders[key];
+  }
+};
+
+app.get("/get-holders", (req, res) => {
+  return res.send({ holders: holders });
+});
+const fetchAccounts = async () => {
+  await findHolders(config.aDescriptor).then((data) => {
+    holders[config.aDescriptor] = data;
+  });
+  await findHolders(config.bDescriptor).then((data) => {
+    holders[config.bDescriptor] = data;
+  });
+};
+setInterval(() => {
+  fetchAccounts();
+}, 1000 * 60 * 10);
+fetchAccounts();
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
